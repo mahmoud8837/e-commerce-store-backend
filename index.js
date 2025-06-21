18d6861c2857d2190d0241bce21686443c560e25
mahmoud8837 <160542473+mahmoud8837@users.noreply.github.com>
2025-06-21 22:31:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5000", "*"],
+    origin: ["http://localhost:5173", "https://e-commerce-storre.netlify.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
