019f3a083d0ff5e01a4d75b6e23ce381425bc8a3
mahmoud8837 <160542473+mahmoud8837@users.noreply.github.com>
2025-06-22 11:07:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get("/api/config/paypal", (req, res) => {
 
 const __dirname = path.resolve();
 
-app.use("/uploads", express.static(path.join(__dirname, '/uploads')));
+app.use("/uploads", express.static(path.join(__dirname, './uploads')));
 
 app.use((error, req, res, next) => {
   return res.status(error.statusCode || 500).json({
